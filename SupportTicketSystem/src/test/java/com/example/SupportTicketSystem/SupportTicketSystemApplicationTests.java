a732dc96322ba1f1e63676582e2e5794d8628f2e
amina4444 <amina.ismailakhunova@alatoo.edu.kg>
2026-04-09 13:52:25 +0600

    Initial commit

---
+++ SupportTicketSystem/src/test/java/com/example/SupportTicketSystem/SupportTicketSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SupportTicketSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
